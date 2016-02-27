0df79e2173e76bb3c67fb8254fd25ef38209ab65
atc <atc@alexcollins.org>
2016-02-27 22:21:52 +0000

    Implement euclidean distance func; add docs

--- src/CF.hs
+++ src/CF.hs
@@ -1,7 +1,9 @@
 module CF
 -- rename to haskdsci
-    ( Sample,
-      manhattan
+    ( Point,
+      Sample,
+      manhattan,
+      euclidean
 --, --      minkowski
     ) where
 
@@ -13,7 +15,14 @@ type Point = (Score, Score)
 
 data Sample a = Rating a [(Item, Score)] deriving (Show, Eq)
 
+-- | The manhattan distance function
 manhattan :: Point -> Point -> Score
-manhattan a@(x1,y1) b@(x2,y2) = (abs (x1 - x2)) + (abs  (y1 - y2))
+manhattan (x1,y1) (x2,y2) = (abs (x1 - x2)) + (abs  (y1 - y2))
 
---minkowski 
+-- | The euclidean distance function
+euclidean :: Point -> Point -> Score
+euclidean (x1,y1) (x2,y2) = sqrt $ xsq + ysq
+  where x = (x1 - x2)
+        xsq = x * x
+        y = (y1 - y2)
+        ysq = y * y
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,8 +5,15 @@ import Test.QuickCheck.All
 
 import CF
 
-prop_Manhattan :: (Float, Float) -> (Float, Float) -> Bool
+prop_Manhattan :: Point -> Point -> Bool
 prop_Manhattan a@(x1,y1) b@(x2,y2) = (manhattan a b) == (abs (x1 - x2)) + (abs  (y1 - y2))
+
+-- This surely isn't sufficient
+prop_EuclidManhattanDiff :: Point -> Point -> Bool
+prop_EuclidManhattanDiff a@(x1,y1) b@(x2,y2) =  someZero || notEqual
+      where notEqual = ((euclidean a b) /= (manhattan a b))
+            summed   = sum [x + y | (x,y) <- [a,b]]
+            someZero = 0.0 == summed
 
 return []
 runTests = $quickCheckAll
