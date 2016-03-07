15c65fe4c33cc3ab7dc5c61315ef4363eb279271
Alex Collins <alextcollins+c9@gmail.com>
2016-03-07 21:00:27 +0000

    Add more tests

--- test/CFSpec.hs
+++ test/CFSpec.hs
@@ -6,19 +6,19 @@ import qualified Data.Map.Strict as M
 import Data.Science.CF
 import Test.Hspec
 
-samplesForAngelica = M.fromList [("Blues Traveler", 3.5 :: Float), 
+samplesForAngelica = M.fromList [("Blues Traveler", 3.5), 
                         ("Broken Bells", 2.0),
                         ("Norah Jones", 4.5),
                         ("Phoenix", 5.0),
                         ("Slightly Stoopid", 1.5),
                         ("The Strokes", 2.5),
-                        ("Vampire Weeekend", 2.0)]
+                        ("Vampire Weekend", 2.0)]
                         
 samplesForHailey = M.fromList [("Broken Bells", 4.0),
                                  ("Deadmau5", 1.0),
                                  ("Norah Jones", 4.0),
                                  ("The Strokes", 4.0),
-                                 ("Vampire Weeekend", 1.0)]
+                                 ("Vampire Weekend", 1.0)]
 
 samplesForVeronica = M.fromList [("Blues Traveler", 3.0),
                                   ("Norah Jones", 5.0),
@@ -40,6 +40,8 @@ samplesForBill = M.fromList [("Blues Traveler", 2.0),
                              ("Phoenix", 2.0),
                              ("Slightly Stoopid", 3.5),
                              ("Vampire Weekend", 3.0)]
+                             
+emptySamples = M.empty
 
 -- "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0,
 -- "Deadmau5": 1.0, "Norah Jones": 3.0,
@@ -60,4 +62,21 @@ specMain = hspec $ do
         it "distanceWith manhattan should correctly compute" $ do
             (distanceWith manhattan samplesForHailey samplesForVeronica) `shouldBe` 2.0
             (distanceWith manhattan samplesForHailey samplesForJordyn) `shouldBe` 7.5
-            (distanceWith manhattan samplesForAngelica samplesForBill) `shouldBe` 9.5
+            (distanceWith manhattan samplesForAngelica samplesForBill) `shouldBe` 9.0
+            
+        it "distanceWith euclidean should correctly compute" $ do
+            (distanceWith euclidean samplesForAngelica samplesForBill) `shouldBe` 4.3
+            
+        it "distanceWith manhattan should result in 0 for no ratings in common" $ do
+            (distanceWith manhattan M.empty M.empty) `shouldBe` 0.0
+            (distanceWith manhattan samplesForAngelica M.empty) `shouldBe` 0.0
+            (distanceWith manhattan M.empty samplesForBill) `shouldBe` 0.0
+            (distanceWith manhattan samplesForAngelica (M.difference samplesForAngelica samplesForBill)) `shouldBe` 0.0
+
+        it "distanceWith euclidean should result in 0 for no ratings in common" $ do
+            (distanceWith euclidean M.empty M.empty) `shouldBe` 0.0
+            (distanceWith euclidean samplesForAngelica M.empty) `shouldBe` 0.0
+            (distanceWith euclidean M.empty samplesForBill) `shouldBe` 0.0
+            (distanceWith euclidean samplesForAngelica (M.difference samplesForAngelica samplesForBill)) `shouldBe` 0.0
+            -- Need to think about 0.0 when no difference, as this leads to misreading nothing in common,
+            -- e.g. (distanceWith euclidean samplesForBill samplesForBill) == 0.0, but they've everything in common
