15d3b8fbeb01a748558bb91708ff0b5c7c4046cd
Alex Collins <alextcollins+c9@gmail.com>
2016-03-09 20:19:28 +0000

    Add property for euclidean

--- test/CFUnitTests.hs
+++ test/CFUnitTests.hs
@@ -9,16 +9,15 @@ import Data.Science.CF
 prop_Manhattan :: Score -> Score -> Bool
 prop_Manhattan a b = (manhattan a b) == abs (a - b)
 
---prop_EuclidManhattanDiff :: Score -> Score -> Bool
---prop_EuclidManhattanDiff a b = sumZero || notEqual
-      --where notEqual = ((euclidean a b) /= (manhattan a b))
-            --sumZero  = 0.0 == a + b
+prop_EuclideanNeverZero :: [Score] -> [Score] -> Bool
+prop_EuclideanNeverZero a b = sumZero || equal || (euclidean a b) >= 0
+      where equal = a == b
+            sumA       = sum a
+            sumB       = sum b
+            sumZero  = 0.0 == sumA + sumB
             
 prop_MinkowskiMatchesManhattan :: Score -> Score -> Bool
 prop_MinkowskiMatchesManhattan a b = (minkowski a b 1) == (manhattan a b)
-      
---prop_MinkowskiMatchesEuclidean :: Score -> Score -> Bool
---prop_MinkowskiMatchesEuclidean a b = (minkowski a b 2) == (euclidean a b)
 
 return []
 runTests = $quickCheckAll
