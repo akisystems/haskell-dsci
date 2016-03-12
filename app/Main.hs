7f796170f66f1c20e812d9b5f1c9007d0c17401c
Alex Collins <alextcollins+c9@gmail.com>
2016-03-12 08:31:26 +0000

    Use optparse-generic to expose commands in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.Science.CF
+import Options.Generic
+
+data Opts = Suggest { dataFile :: String, user :: String} -- the recommendation
+          | Blah    { bob :: String }
+    deriving (Generic, Show)
+    
+instance ParseRecord Opts
+
+doSuggest :: String -> String -> IO ()
+doSuggest file user = do
+    contents <- readFile file
+    let lines' = lines contents
+    mapM_ putStrLn lines'
 
 main :: IO ()
-main = putStrLn "To do"
+main = do
+    opts <- getRecord "dsci"
+    case opts of
+        (Suggest f u) -> doSuggest f u
+        _ -> putStrLn "What?"
