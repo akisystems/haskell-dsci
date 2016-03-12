3d0097469a578ecf1326f4288c907a7e8042d036
Alex Collins <alextcollins+c9@gmail.com>
2016-03-12 12:24:59 +0000

    Basic command support & recommendation interface

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,21 @@ import Options.Generic
 data Opts = Suggest { dataFile :: String, user :: String} -- the recommendation
           | Blah    { bob :: String }
     deriving (Generic, Show)
-    
+
 instance ParseRecord Opts
+
+toSample :: String -> Sample String String
+toSample line = error "lol"
 
 doSuggest :: String -> String -> IO ()
 doSuggest file user = do
     contents <- readFile file
     let lines' = lines contents
-    mapM_ putStrLn lines'
+    let samples = map toSample lines'
+    let userSample = findUser user samples
+    let recs = recommend euclidean userSample samples
+    mapM_ printRec recs
+    where printRec (item, sc) = putStrLn ("Item: " ++ item ++ ", Score: " ++ (show sc))
 
 main :: IO ()
 main = do
